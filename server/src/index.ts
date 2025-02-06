6a61249293ecc4d3478be195091fc822b8564bb2
Kamblossi <newton.ochieng97@gmail.com>
2025-02-06 22:32:20 +0300

    server side api controllers added

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,10 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+/* ROUTE IMPORTS */
+import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes";
+
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -21,6 +25,11 @@ app.get("/", (req, res) => {
     res.send("This is home route");
   });
 
+
+  app.use("/projects", projectRoutes);
+  app.use("/tasks", taskRoutes);
+  
+  
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
 app.listen(port, "0.0.0.0", () => {
